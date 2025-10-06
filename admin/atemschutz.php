4082a054e670a8ff70e51b03ecdfa051ce4679f4
Daniel Leuchtenberg <dleuchtenberg89@gmail.com>
2025-10-06 15:15:07 +0200

    fix(atemschutz): 'Ãœbung planen' als Link mit href #planTrainingModal + data-bs-* fÃ¼r maximale Bootstrap-KompatibilitÃ¤t

--- admin/atemschutz.php
+++ admin/atemschutz.php
@@ -225,10 +225,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
                 </a>
             </div>
             <div class="col-12 col-md-6">
-                <button class="btn btn-outline-success w-100 py-4" id="btnPlanTraining" data-bs-toggle="modal" data-bs-target="#planTrainingModal">
+                <a class="btn btn-outline-success w-100 py-4" id="btnPlanTraining" href="#planTrainingModal" data-bs-toggle="modal" data-bs-target="#planTrainingModal">
                     <i class="fas fa-calendar-plus fa-2x mb-2 d-block"></i>
                     <span class="fs-5">Übung planen</span>
-                </button>
+                </a>
             </div>
             <div class="col-12 col-md-6">
                 <button class="btn btn-outline-secondary w-100 py-4" id="btnRecordData">
