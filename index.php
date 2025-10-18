6c94537b30f11d47c1d22266ffec8cf5c3c1cebc
Daniel Leuchtenberg <dleuchtenberg89@gmail.com>
2025-10-18 09:30:20 +0200

    Versionsnummer auf 2.1 aktualisiert
    - Footer-Version von 1.0 auf 2.1 geÃ¤ndert
    - Reflektiert die umfangreichen Verbesserungen der App
    - Atemschutz-Funktionen, schÃ¶ne E-Mails, verbesserte UI

--- index.php
+++ index.php
@@ -211,7 +211,7 @@ require_once 'includes/functions.php';
 
     <footer class="bg-light mt-5 py-4">
         <div class="container text-center">
-            <p class="text-muted mb-0">&copy; 2025 Boedes Feuerwehr App&nbsp;&nbsp;Version: 1.0&nbsp;&nbsp;Alle Rechte vorbehalten</p>
+            <p class="text-muted mb-0">&copy; 2025 Boedes Feuerwehr App&nbsp;&nbsp;Version: 2.1&nbsp;&nbsp;Alle Rechte vorbehalten</p>
         </div>
     </footer>
 
